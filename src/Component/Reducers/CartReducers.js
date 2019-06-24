c429bdbc91c290673600c757fcea9fbca051cfb5
Dewangan <dneelam@amazon.com>
2019-06-25 01:41:32 +0530

    third commit

--- src/Component/Reducers/CartReducers.js
+++ src/Component/Reducers/CartReducers.js
@@ -11,10 +11,10 @@ const initState = {
     items: [
         {id:1,title:'Winter body', desc: "Casual sneakers for men", price:110,img:Item1},
         {id:2,title:'Adidas', desc: "Sports shoes for Men, Running, Casual, Gymwear", price:80,img: Item2},
-        {id:3,title:'Vans', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.",price:120,img: Item3},
-        {id:4,title:'White', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price:260,img:Item4},
-        {id:5,title:'Cropped-sho', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price:160,img: Item5},
-        {id:6,title:'Blues', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.",price:90,img: Item6}
+        {id:3,title:'Vans', desc: "xyz",price:120,img: Item3},
+        {id:4,title:'White', desc: "abc", price:260,img:Item4},
+        {id:5,title:'Cropped-sho', desc: "xxxxxx", price:160,img: Item5},
+        {id:6,title:'Blues', desc: "yyyyyy",price:90,img: Item6}
     ],
     addedItems:[],
     total: 0
